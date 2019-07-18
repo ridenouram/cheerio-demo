020a5a594017a2a076da45e9572b4858064d18a2
Anna Ridenour <annamridenour@gmail.com>
2019-07-18 11:10:45 -0700

    cheerio working

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const cheerio = require('cheerio');
 const request = require('request');
 
-
+request({
+  method: 'GET',
+  url: 'https://twinpeaks.fandom.com/wiki/Audrey_Horne',
+}, (err, res, body) => {
+  if(err) return console.error(err);
+  let $ = cheerio.load(body);
+  // let character = $('<h2 class="pi-item pi-item-spacing pi-title" data-source="Title">Audrey Horne</h2>');
+  // console.log(character.text());
+});
